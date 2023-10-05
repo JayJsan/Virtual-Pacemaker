9a4dacc5843fae67974976340ad0527b607f7b49
Job Josef Santos <job.santos924@gmail.com>
2023-10-05 14:43:15 +1300

    feat: add labs

---
+++ Lab2/DE2-115/software/cs303_lab2/obj/default/main.d
@@ -0,0 +1,17 @@
+obj/default/main.o: main.c ../cs303_lab2_bsp/system.h \
+ ../cs303_lab2_bsp/linker.h \
+ ../cs303_lab2_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../cs303_lab2_bsp//HAL/inc/io.h ../cs303_lab2_bsp//HAL/inc/alt_types.h \
+ sccharts.h
+
+../cs303_lab2_bsp/system.h:
+
+../cs303_lab2_bsp/linker.h:
+
+../cs303_lab2_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../cs303_lab2_bsp//HAL/inc/io.h:
+
+../cs303_lab2_bsp//HAL/inc/alt_types.h:
+
+sccharts.h:
